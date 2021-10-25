53faee6f2632d5c629f441626df00c19652d2de9
frants-jeon <jjh6963@naver.com>
2021-10-25 18:07:04 +0900

    try_1

---
+++ python_solved/level_9_basic_math2/baekjoon_9-6_9020.py
@@ -0,0 +1,44 @@
+import sys
+
+N = int(input())
+num = []
+for _ in range(N):
+    num.append(int(sys.stdin.readline()))
+
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+def get_primes(m, n):
+    numbers = set(range(n, 1, -1))
+    prime_number = []
+    for i in range(len(numbers)):
+        p = list(numbers)[i]
+        numbers.difference_update(set(range(p*2, n+1, p)))
+        if p >= m:
+            prime_number.append(p)
+        if len(numbers) - 1 == i: break
+    return prime_number
+
+prime_number = sorted(get_primes(0, 10000))
+
+for i in num:
+    if is_prime(i // 2):
+        print(i // 2, i // 2)
+    else:
+        for j in prime_number:
+            if j > i // 2:
+                big_prime = j
+                small_prime = i - big_prime
+                if is_prime(small_prime):
+                    print(small_prime, big_prime)
+                    break
+        
