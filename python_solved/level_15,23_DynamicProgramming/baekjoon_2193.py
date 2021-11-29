8c016961603f937942d22a93e43cb3975701d3e4
frants-jeon <jjh6963@naver.com>
2021-11-29 23:46:20 +0900

    update

---
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_2193.py
@@ -0,0 +1,16 @@
+##### 이친수 #####
+from sys import stdin
+
+N = int(stdin.readline())
+dp = [[-1, -1] for _ in range(N)]
+
+def f(n, s):
+  if(dp[n][s] != -1): return dp[n][s]
+  if n == 0: return 1
+  if s == 0:
+    dp[n][s] = f(n - 1, 0) + f(n - 1, 1)
+  else: dp[n][s] = f(n - 1, 0)
+  
+  return dp[n][s]
+
+print(f(N - 1, 1))
