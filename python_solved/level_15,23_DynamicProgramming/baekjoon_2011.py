f4693ec8027eac8c3f743efc0f388bff046fe2ff
frants-jeon <jjh6963@naver.com>
2021-12-26 23:59:11 +0900

    fail

---
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_2011.py
@@ -0,0 +1,22 @@
+##### 암호 #####
+from sys import stdin
+secret_code = stdin.readline().strip()
+s_len, dp = len(secret_code), [0] * len(secret_code)
+if secret_code[0] == '0': print(0), exit()
+elif s_len > 1:
+  if (int(secret_code[0]) == 2 and 0 < int(secret_code[1]) < 7) \
+    or (int(secret_code[0]) == 1 and int(secret_code[1]) != 0):
+    dp[0], dp[1] = 1, 2
+  elif int(secret_code[0]) > 2 and int(secret_code[1]) == 0:
+    print(0), exit()
+  else: dp[0], dp[1] = 1, 1
+else: print(1), exit()
+for i in range(2, s_len):
+  check = int(secret_code[i - 1])
+  if int(secret_code[i]) == 0:
+    if check > 2 or check == 0: print(0), exit()
+    else: dp[i] = dp[i - 2] % 1000000
+  elif 0 < check <= 2 and int(secret_code[i]) < 7:
+    dp[i] = dp[i - 2] + dp[i - 1]
+  else: dp[i] = dp[i - 1] % 1000000
+print(dp[-1])
