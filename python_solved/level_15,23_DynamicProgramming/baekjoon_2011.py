7290f3a73c62a3ffc5efac79dc51c88f44227b43
frants-jeon <jjh6963@naver.com>
2021-12-27 02:26:45 +0900

    modify

--- python_solved/level_15,23_DynamicProgramming/baekjoon_2011.py
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_2011.py
@@ -1,22 +1,22 @@
 ##### 암호 #####
-from sys import stdin
+from sys import stdin, exit
 secret_code = stdin.readline().strip()
 s_len, dp = len(secret_code), [0] * len(secret_code)
-if secret_code[0] == '0': print(0), exit()
+if secret_code[0] == '0': print(0), exit(0)
 elif s_len > 1:
   if (int(secret_code[0]) == 2 and 0 < int(secret_code[1]) < 7) \
     or (int(secret_code[0]) == 1 and int(secret_code[1]) != 0):
     dp[0], dp[1] = 1, 2
   elif int(secret_code[0]) > 2 and int(secret_code[1]) == 0:
-    print(0), exit()
+    print(0), exit(0)
   else: dp[0], dp[1] = 1, 1
 else: print(1), exit()
 for i in range(2, s_len):
   check = int(secret_code[i - 1])
   if int(secret_code[i]) == 0:
-    if check > 2 or check == 0: print(0), exit()
+    if check > 2 or check == 0: print(0), exit(0)
     else: dp[i] = dp[i - 2] % 1000000
-  elif 0 < check <= 2 and int(secret_code[i]) < 7:
+  elif (check == 2 and 0 < int(secret_code[i]) < 7) or (check == 1 and int(secret_code[i]) != 0):
     dp[i] = dp[i - 2] + dp[i - 1]
   else: dp[i] = dp[i - 1] % 1000000
 print(dp[-1])
