4f96845a6978e1367a3e99051370dbd69f32c04e
frants-jeon <jjh6963@naver.com>
2021-12-27 16:08:33 +0900

    modify

--- python_solved/level_15,23_DynamicProgramming/baekjoon_2011.py
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_2011.py
@@ -10,13 +10,13 @@ elif s_len > 1:
   elif int(secret_code[0]) > 2 and int(secret_code[1]) == 0:
     print(0), exit(0)
   else: dp[0], dp[1] = 1, 1
-else: print(1), exit()
+else: print(1), exit(0)
 for i in range(2, s_len):
   check = int(secret_code[i - 1])
   if int(secret_code[i]) == 0:
     if check > 2 or check == 0: print(0), exit(0)
     else: dp[i] = dp[i - 2] % 1000000
   elif (check == 2 and 0 < int(secret_code[i]) < 7) or (check == 1 and int(secret_code[i]) != 0):
-    dp[i] = dp[i - 2] + dp[i - 1]
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000
   else: dp[i] = dp[i - 1] % 1000000
 print(dp[-1])
---
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_9095.py
@@ -0,0 +1,11 @@
+##### 1,2,3 더하기 #####
+from sys import stdin
+input = stdin.readline
+T = int(input())
+dp = [0 for _ in range(11)]
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, 11):
+  dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+for _ in range(T):
+  n = int(input())
+  print(dp[n])
