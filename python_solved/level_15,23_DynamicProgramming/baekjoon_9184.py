17c2f54221e82da840a29c8ebfd990efab99525c
frants-jeon <jjh6963@naver.com>
2022-02-21 10:56:30 +0900

    modify

--- python_solved/level_15,23_DynamicProgramming/baekjoon_9184.py
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_9184.py
@@ -6,7 +6,7 @@ dp = [[[0 for _ in range(21)] for _ in range(21)] for _ in range(21)]
 def w(a, b, c):
   if a <= 0 or b <= 0 or c <= 0:
     return 1
-  if a > 20 and b > 20 or c > 20:
+  if a > 20 or b > 20 or c > 20:
     dp[20][20][20] = w(20, 20, 20)
     return dp[20][20][20]
   if dp[a][b][c] != 0: return dp[a][b][c]
