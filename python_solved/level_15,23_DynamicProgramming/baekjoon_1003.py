4f7c11142cf8baf370b7b0eef6942911386935d7
frants-jeon <jjh6963@naver.com>
2022-02-22 15:01:00 +0900

    dp (add top_down)

--- python_solved/level_15,23_DynamicProgramming/baekjoon_1003.py
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_1003.py
@@ -1,14 +1,38 @@
 ##### 피보나치 함수 #####
 from sys import stdin
 input = stdin.readline
-T = int(input())
-dp = [[-1, -1] for _ in range(41)]
-dp[0], dp[1] = [1, 0], [0, 1]
-for i in range(2, 41):
-  if dp[i - 1] != [-1, -1] and dp[i - 2] != [-1, -1]:
-    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
-    dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
 
-for _ in range(T):
-  N = int(input())
-  print(dp[N][0], dp[N][1])
+def bottom_up():
+  T = int(input())
+  dp = [[-1, -1] for _ in range(41)]
+  dp[0], dp[1] = [1, 0], [0, 1]
+  for i in range(2, 41):
+    if dp[i - 1] != [-1, -1] and dp[i - 2] != [-1, -1]:
+      dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
+      dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
+
+  for _ in range(T):
+    N = int(input())
+    print(dp[N][0], dp[N][1])
+
+
+def top_down():
+  dp = [[-1, -1] for _ in range(41)]
+  def memoization(n):
+    if dp[n] != [-1, -1]: return dp[n]
+    if n == 0:
+      dp[n] = [1, 0]
+      return dp[n]
+    if n == 1:
+      dp[n] = [0, 1]
+      return dp[n]
+    
+    b1 = memoization(n - 1)
+    b2 = memoization(n - 2)
+    dp[n][0] = b1[0] + b2[0]
+    dp[n][1] = b1[1] + b2[1]
+    return dp[n]
+  
+  for tc in range(int(input())):
+    answer = memoization(int(input()))
+    print(answer[0], answer[1])
--- python_solved/level_15,23_DynamicProgramming/baekjoon_11053.py
+++ python_solved/level_15,23_DynamicProgramming/baekjoon_11053.py
@@ -1,14 +1,40 @@
 ##### 가장 긴 증가하는 부분 수열 #####
 from sys import stdin
 input = stdin.readline
-N = int(input())
-arr = list(map(int,input().split()))
-dp, dp[0] = [0] * N, 1
-for i in range(1, N):
-  tmp = []
-  for j in range(i):
-    if arr[j] < arr[i]:
-      tmp.append(dp[j] + 1)
-  if not tmp: dp[i] = 1
-  else: dp[i] = max(tmp)
-print(max(dp))
+
+def bottom_up():
+  N = int(input())
+  arr = list(map(int,input().split()))
+  dp, dp[0] = [0] * N, 1
+  for i in range(1, N):
+    tmp = []
+    for j in range(i):
+      if arr[j] < arr[i]:
+        tmp.append(dp[j] + 1)
+    if not tmp: dp[i] = 1
+    else: dp[i] = max(tmp)
+  print(max(dp))
+
+
+def top_down():
+  N = int(input())
+  arr = list(map(int,input().split()))
+  dp = [0] * N
+  def memoization(n):
+    if dp[n] != 0: return dp[n]
+    if n == 0:
+      dp[n] = 1
+      return 1
+    tmp = []
+    for i in range(n - 1, -1, -1):
+      if arr[i] < arr[n]:
+        tmp.append(memoization(i))
+    if tmp:
+      dp[n] = max(tmp) + 1
+    else: dp[n] = 1
+    return dp[n]
+  # dp값 안구하고 건너뛰는 경우도 있어서 반복문으로 다 구해줌.
+  for i in range(N - 1, -1, -1):
+    if dp[i] == 0:
+      memoization(i)
+  print(max(dp))
