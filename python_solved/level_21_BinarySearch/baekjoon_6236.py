0a647c0492c52c0ede70f991263bfb6e994a9bc2
frants-jeon <jjh6963@naver.com>
2022-03-16 18:56:25 +0900

    modify (high값 재설정, 주석 추가)

--- python_solved/level_21_BinarySearch/baekjoon_6236.py
+++ python_solved/level_21_BinarySearch/baekjoon_6236.py
@@ -2,23 +2,29 @@
 from sys import stdin
 input = stdin.readline
 N, M = map(int,input().split())
-arr = [int(input()) for _ in range(N)]
-low, high = 0, 100000
+days = [int(input()) for _ in range(N)]
+low, high = 0, 1000000000
 while low + 1 < high:
-  middle = (low + high) // 2
-  current_money = middle
-  cnt = 1
-  sizeDown = 1
+  middle = (low + high) // 2 # 인출한 돈
+  current_money = middle # 현재 잔액
+  cnt = 1 # 인출 횟수
+  sizeDown = 1 # high값을 낮춰야 하는지 여부
   for i in range(N):
-    if arr[i] > middle: sizeDown = 0
-    if current_money - arr[i] >= 0:
-      current_money -= arr[i]
-    else:
-      current_money = middle - arr[i]
+    # i번째 날에 써야할 돈이 출금한 금액보다 많다면 break하고 출금액을 높임
+    if days[i] > middle:
+      sizeDown = 0
+      break
+    # 오늘 쓸 금액이 충분하다면 현재 잔액에서 빼줌
+    elif current_money - days[i] >= 0:
+      current_money -= days[i]
+    else: # 추가로 출금해야 한다면 인출횟수 늘리고 현재잔액 갱신
+      current_money = middle - days[i]
       cnt += 1
-  if sizeDown == 0: low = middle
-  elif cnt <= M:
-    high = middle
-  else:
+  # 사이즈 줄여야하지 않으면 low값 갱신
+  if sizeDown == 0 or cnt > M:
     low = middle
+  # 사이즈 줄여야하고 인출횟수가 적으면 high값 갱신
+  else:
+    high = middle
+
 print(high)
